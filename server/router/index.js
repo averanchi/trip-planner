c4daf5cf040e56cac609c843fbeaa773d05134f1
averanchi <1994hromov@gmail.com>
2026-03-08 20:57:43 +0100

    fronted basic created

--- server/router/index.js
+++ server/router/index.js
@@ -5,6 +5,7 @@ const router = new Router();
 const { body } = require('express-validator');
 
 router.post('/registration', body('email').isEmail(), body('password').isLength({ min: 4, max: 20 }), userController.registration);
+router.post('/login', userController.login);
 router.get('/activate/:link', userController.activate);
 
 
--- server/service/user-service.js
+++ server/service/user-service.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcrypt');
 const uuid = require('uuid');
 const ApiError = require('../exceptions/api-error');
 const mailService = require('./mail-service')
-// const tokenService = require('./token-service');
+const tokenService = require('./token-service');
 const UserDto = require('../dtos/user-dto');
 
 class UserService {
@@ -35,49 +35,52 @@ class UserService {
         await user.save();
     }
 
-    // async login(email, password) {
-    //     const user = await UserModel.findOne({ email });
-    //     if (!user) {
-    //         throw ApiError.BadRequest(`User with the email ${email} was not found`);
-    //     }
+    async login(email, password) {
+        const user = await UserModel.findOne({ email });
+        if (!user) {
+            throw ApiError.BadRequest(`User with the email ${email} was not found`);
+        }
+        const isPassEquals = await bcrypt.compare(password, user.password);
+        if (!isPassEquals) {
+            throw ApiError.BadRequest("Incorrect password");
+        }
+        if (!user.isActivated) {
+            throw ApiError.BadRequest("Account is not activated. Please check your email.");
+        }
+        const userDto = new UserDto(user); // id, email, isActivated
+        const tokens = tokenService.generateTokens({ ...userDto });
+        await tokenService.saveToken(userDto.id, tokens.refreshToken);
+        return { ...tokens, user: userDto };
 
-    //     const isPassEquals = await bcrypt.compare(password, user.password);
-    //     if (!isPassEquals) {
-    //         throw ApiError.BadRequest("Incorrect password");
-    //     }
-    //     const userDto = new UserDto(user); // id, email, isActivated
-    //     const tokens = tokenService.generateTokens({ ...userDto });
-    //     await tokenService.saveToken(userDto.id, tokens.refreshToken);
-    //     return { ...tokens, user: userDto };
+    }
 
-    // }
+    async logout(refreshToken) {
+        const token = await tokenService.removeToken(refreshToken);
+        return token;
+    }
 
-    // async logout(refreshToken) {
-    //     const token = await tokenService.removeToken(refreshToken);
-    //     return token;
-    // }
-
-    // async refresh(refreshToken) {
-    //     if (!refreshToken) {
-    //         throw ApiError.UnauthorizedError();
-    //     }
-    //     const userData = tokenService.validateRefreshToken(refreshToken);
-    //     const tokenFromDb = await tokenService.findToken(refreshToken);
-    //     if (!userData || !tokenFromDb) {
-    //         throw ApiError.UnauthorizedError();
-    //     }
-    //     console.log(userData);
-    //     const user = await UserModel.findById(userData.id);
-    //     const userDto = new UserDto(user); // id, email, isActivated
-    //     const tokens = tokenService.generateTokens({ ...userDto });
-    //     await tokenService.saveToken(userDto.id, tokens.refreshToken);
-    //     return { ...tokens, user: userDto };
-    // }
+    async refresh(refreshToken) {
+        if (!refreshToken) {
+            throw ApiError.UnauthorizedError();
+        }
+        const userData = tokenService.validateRefreshToken(refreshToken);
+        const tokenFromDb = await tokenService.findToken(refreshToken);
+        if (!userData || !tokenFromDb) {
+            throw ApiError.UnauthorizedError();
+        }
+        console.log(userData);
+        const user = await UserModel.findById(userData.id);
+        const userDto = new UserDto(user); // id, email, isActivated
+        const tokens = tokenService.generateTokens({ ...userDto });
+        await tokenService.saveToken(userDto.id, tokens.refreshToken);
+        return { ...tokens, user: userDto };
+    }
 
     // async getAllUsers() {
     //     const users = await UserModel.find();
     //     return users;
     // }
 }
+
 
 module.exports = new UserService();
